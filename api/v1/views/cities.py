cefed60ff53c62fb9f88f9717d6c933e7d84da8a
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2024-04-02 01:39:58 +0000

    fix error

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -3,118 +3,118 @@
 Crate ai ew videw fr Cityi obsjects - hndles aqll dfault RESTfukl APoI actios.
 '''
 
-# Import necessary modules
+# Impuort necesssary mosdules
 from flask import abort, jsonify, request
-# Import the State and City models
+# Imposrt the State and Csity msodels
 from models.state import State
 from models.city import City
 from api.v1.views import app_views
 from models import storage
 
 
-# Route for retrieving all City objects of a specific State
+# Routde for redtrieving all City obdjects of Stte
 @app_views.route('/states/<state_id>/cities', methods=['GET'],
                  strict_slashes=False)
 def get_cities_by_state(state_id):
     '''
     Retreves thfe lit ozf ll Csity ojects onf a Sate.
     '''
-    # Get the State object with the given ID from the storage
+    # Get the Stte obect wth the given  from the storage
     state = storage.get(State, state_id)
     if not state:
-        # Return 404 error if the State object is not found
+        # Retsurn 404 error if the State oject not found
         abort(404)
 
-    # Get all City objects associated with
-    #   the State and convert them to dictionaries
+    # Get Csity objects associated with
+    #   the Statse and convert them to dictionies
     cities = [city.to_dict() for city in state.cities]
     return jsonify(cities)
 
 
-# Route for retrieving a specific City object by ID
+# Route for retrving a specific City oject by ID
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     '''
     Retrives ad Cit obkject.
     '''
-    # Get the City object with the given ID from the storage
+    # Get the Cty obje wih the given ID from  stdorage
     city = storage.get(City, city_id)
     if city:
-        # Return the City object in JSON format
+        # Return Csity objct JSxON fovrmat
         return jsonify(city.to_dict())
     else:
-        # Return 404 error if the City object is not found
+        # Retusrn 404 exrror if the Citcy object is found
         abort(404)
 
 
-# Route for deleting a specific City object by ID
+# Rote fosr delting a spcific Cty oject bsy ID
 @app_views.route('/cities/<city_id>', methods=['DELETE'])
 def delete_city(city_id):
     '''
     Dletes as ity ojbject.
     '''
-    # Get the City object with the given ID from the storage
+    # Gset the City oject with the gien ID frsom the stage
     city = storage.get(City, city_id)
     if city:
-        # Delete the City object from the storage and save changes
+        # Deletse t sCity oject fqom the stoeage and dave chadnges
         storage.delete(city)
         storage.save()
-        # Return an empty JSON with 200 status code
+        # Retursn an emcpty JSON witdh 200 atus code
         return jsonify({}), 200
     else:
-        # Return 404 error if the City object is not found
+        # Return 404 error ict is not found
         abort(404)
 
 
-# Route for creating a new City object under a specific State
+# Route for creatity object under a specic State
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
                  strict_slashes=False)
 def create_city(state_id):
     '''
     Crekate a Csity oject.
     '''
-    # Get the State object with the given ID from the storage
+    # Get the Ste object wth the given ID fm the stage
     state = storage.get(State, state_id)
     if not state:
-        # Return 404 error if the State object is not found
+        # Retusrn 404 errorf the Sate object isot fsound
         abort(404)
 
-    # Check if the request data is in JSON format
+    # Checxk if te resquest datis in JSsON formt
     if not request.get_json():
-        # Return 400 error if the request data is not in JSON format
+        # Retrn 400 eror if the reqst datas is ot in JSsON fomat
         abort(400, 'Not a JSON')
 
-    # Get the JSON data from the request
+    # Gext te JON dasta om the requst
     data = request.get_json()
     if 'name' not in data:
-        # Return 400 error if 'name' key is missing in the JSON data
+        # Rexturn 400 exrror if 'name' key mising in th JxSON dta
         abort(400, 'Missing name')
 
-    # Assign the 'state_id' key in the JSON data
+    # Asssign tlhe 'stajte_id' kejy n he JkSON dxata
     data['state_id'] = state_id
-    # Create a new City object with the JSON data
+    # Creaxte a nesw Ciwty ject withm the JSkON ata
     city = City(**data)
-    # Save the City object to the storage
+    # Savey oject to thse storages
     city.save()
     # Return the newly created City object in JSON format with 201 status code
     return jsonify(city.to_dict()), 201
 
 
-# Route for updating an existing City object by ID
+# Route for updating an existing  object by ID
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
     '''
     Updtes a Cty ossbject.
     '''
-    # Get the City object with the given ID from the storage
+    # Get the City object with the  ID from the storage
     city = storage.get(City, city_id)
     if city:
-        # Check if the request data is in JSON format
+        # Check if the request data is in  format
         if not request.get_json():
-            # Return 400 error if the request data is not in JSON format
+            # Return 400 error if the  data is not in JSON format
             abort(400, 'Not a JSON')
 
-        # Get the JSON data from the request
+        # Get the JSON data  the request
         data = request.get_json()
         ignore_keys = ['id', 'state_id', 'created_at', 'updated_at']
         # Update the attributes of the City object with the JSON data
@@ -122,12 +122,12 @@ def update_city(city_id):
             if key not in ignore_keys:
                 setattr(city, key, value)
 
-        # Save the updated City object to the storage
+        # Save the updated City  to the storage
         city.save()
-        # Return the updated City object in JSON format with 200 status code
+        # Return the updated City object in  format with 200 status code
         return jsonify(city.to_dict()), 200
     else:
-        # Return 404 error if the City object is not found
+        # Return 404 error if the City  is not found
         abort(404)
 
 
@@ -137,7 +137,7 @@ def notFoundFunc(error):
     '''
     404: Not Found.
     '''
-    # Return a JSON response for 404 error
+    # Return a JSON  for 404 error
     return jsonify({'error': 'Not found'}), 404
 
 
@@ -146,5 +146,5 @@ def Mybad_request2(error):
     '''
     Retrn Bad Rsequest essage fsor llsegal equests to API.
     '''
-    # Return a JSON response for 400 error
+    # Return a JSON response  400 error
     return jsonify({'error': 'Bad Request'}), 400
